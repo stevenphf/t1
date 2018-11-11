f8c8e15f3d1eccc6db70cb6eb972d6ae7913ce37
Steven <stevenphf@126.com>
2018-11-11 11:04:15 +0800

    Update t.py
    python t.py <line>
    从train.csv挑一行转换为图。

--- t.py
+++ t.py
@@ -1,3 +1,14 @@
-from __future__ import print_function
+import cv2,sys
+import pandas as pd
 
-print("P写好玩的小应用.")
+lines = sys.argv[1:]
+
+train = pd.read_csv('train.csv')
+
+for line in lines:
+    if int(line)>1:
+        x_train = train.iloc[int(line)-2, 1:].values  #实际上取标号为line的行
+        y_label = train.iloc[int(line)-2, :].values[0]
+        x_train.shape = -1, 28
+        cv2.imwrite("temp.jpg", x_train)
+        img = cv2.imread("temp.jpg")
